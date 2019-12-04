9e6fcda4e67a7b297c8fcfcec2583d36f6eed33f
Jasmin <jasymatos@gmail.com>
2019-12-04 11:17:46 -0500

    added new gatsby app with basic blog set up

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Dog-Blog
+
+### Personal blog focused on dog owners in the St. Petersburg, FL area.
+
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Welcome to my website</h1>
+    <p>Personal blog focused on dog owners in the St. Petersburg, FL area</p>
+  </div>
+)
+
+export default IndexPage
